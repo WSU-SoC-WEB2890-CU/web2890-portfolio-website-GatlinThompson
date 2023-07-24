94769b4513bb20aa4bbe101e40b70eb973ad391c
GatlinThompson <112907390+GatlinThompson@users.noreply.github.com>
2023-07-24 00:07:06 -0600

    Final submission

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,2 +1,21 @@
 //TODO - Your ES6 JavaScript code (if any) goes here
 import "bootstrap"
+
+//Navbar Code
+let navShow = false
+
+const navButton = document.getElementById("navbar-toggler")
+navButton.addEventListener("click", () => {
+  navBackground()
+})
+
+function navBackground() {
+  const background = document.getElementById("nav-background")
+  if (!navShow) {
+    background.classList.add("clicked")
+    navShow = true
+  } else {
+    background.classList.remove("clicked")
+    navShow = false
+  }
+}
